f637a378cf5b31d96a8cd3969d5de13766ebd1ea
sturdy3299 <c6k6z0zf@anonaddy.me>
2025-06-20 15:04:24 +0200

    Update overlay template

--- overlay.nix
+++ overlay.nix
@@ -1,3 +1,10 @@
-self: super: {
-  steuern-lu-2024nP = super.callPackage /default.nix { };
+self: super:
+let
+  src = builtins.fetchGit {
+    url = "https://github.com/myspace7164/steuern-lu-2024nP.git";
+    ref = "main";
+  };
+
+in {
+  steuern-lu-2024nP = super.callPackage (src + "/default.nix") { };
 }
